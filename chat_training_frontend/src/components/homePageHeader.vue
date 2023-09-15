0344d5aa990bea40381ae40baa3e13c9b6af37f8
awab34 <awab3468@gmail.com>
2023-09-15 23:36:33 +0400

    nav bar updated

--- chat_training_frontend/src/components/homePageHeader.vue
+++ chat_training_frontend/src/components/homePageHeader.vue
@@ -35,13 +35,13 @@ async function logout(){
   <main>
     <nav class="navbar navbar-dark cardsColor fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand headerColor" href="#">header</a>
+    <a class="navbar-brand headerColor" href="/home">Letaatafo</a>
     <button class="navbar-toggler headerColor" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Letaarafo</h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
@@ -49,8 +49,27 @@ async function logout(){
           <li class="nav-item">
             <a :class="'nav-link ' +( activePage == '1'? 'active' :'')" aria-current="page" href="/home">Home</a>
           </li>
-          <li class="nav-item">
-            <a :class="'nav-link ' +( activePage == '0'? 'active' :'')"  href="/userinfo">Account Info</a>
+          
+          <li v-if="activePage == '0'" class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Account Info
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><a class="dropdown-item text-center" href="/updateUser">Update Account</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item text-center" href="/resetpassword">Reset Password</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item text-center" href="/change-email">Change Email</a></li>
+              
+              
+            </ul>
+          </li>
+          <li v-else class="nav-item">
+            <a :class="'nav-link '"  href="/userinfo">Account Info</a>
           </li>
           <li class="nav-item">
             <a :class="'nav-link '" v-if="activePage == '0'" href="/resetpassword">Reset Password</a>
